ef6d8685f12b10ac35be3a94db8ca626bd691657
Aanisha Newaz <90078094+Aanisha02@users.noreply.github.com>
2022-04-14 22:58:21 -0400

    Added comments into UIConstants.java

--- src/main/java/csci2020u/finalproject/tictactoe/UIConstants.java
+++ src/main/java/csci2020u/finalproject/tictactoe/UIConstants.java
@@ -1,5 +1,17 @@
-package csci2020u.finalproject.tictactoe;
+/**
+ * @author Japnit Ahuja
+ * @author Aanisha Newaz
+ * @author Chioma Okechukwu
+ * @author Jessica Patel
+ *
+ * @version 1.0
+ */
 
+package com.example.finalproject;
+
+/**
+* Constants to finalize size of application
+*/
 public class UIConstants {
     public static final int APP_WIDTH = 600;
     public static final int APP_HEIGHT = 600;
