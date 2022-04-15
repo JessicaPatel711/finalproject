caf4fcbec8133cb98c9007e6d775c7f0f85671fc
Aanisha Newaz <90078094+Aanisha02@users.noreply.github.com>
2022-04-14 22:43:24 -0400

    Added comments in InfoCenter.java

--- src/main/java/com/example/finalproject/InfoCenter.java
+++ src/main/java/com/example/finalproject/InfoCenter.java
@@ -1,3 +1,12 @@
+/**
+ * @author Japnit Ahuja
+ * @author Aanisha Newaz
+ * @author Chioma Okechukwu
+ * @author Jessica Patel
+ *
+ * @version 1.0
+ */
+
 package com.example.finalproject;
 
 import javafx.geometry.Pos;
@@ -7,11 +16,16 @@ import javafx.scene.text.Font;
 
 /**
  * The info center is the area above the tile board
+ *
+ * InfoCentre is the part of the program UI above the X and O tile board where the players play. 
+ * It shows up to date information and updates on the gameplay, for example, announcing the winner.
  */
 public class InfoCenter {
     private StackPane pane;
     private Label message;
-
+    /**
+    * Class constructor 
+    */
     public InfoCenter() {
         pane = new StackPane();
         pane.setMinSize(UIConstants.APP_WIDTH, UIConstants.INFO_CENTER_HEIGHT);
@@ -29,11 +43,16 @@ public class InfoCenter {
         pane.getChildren().add(message);
     }
 
-    // getters
+    /**
+    * Getter method to access stack pane
+    */
     public StackPane getStackPane() {
         return pane;
     }
 
+    /**
+    * setter method to set message to announce game winner or tie decision
+    */
     public void setInfoCenterMessage(String message) {
         this.message.setText(message);
     }
